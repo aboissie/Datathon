c3877e2b9fefdabb3bd7a9f79426246050aac9ce
anujshetty <anujshetty248@gmail.com>
2023-03-11 15:22:08 -0800

    Logistic regression

--- features.py
+++ features.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def get_coord_mask(df):
+def get_coord_mask_params(df):
     lat_min = df['lat'].min()
     lat_max = df['lat'].max()
     lng_min = df['lng'].min()
@@ -9,7 +9,9 @@ def get_coord_mask(df):
     # lat_window, lng_window = 0.1, 0.1
     lat_window = (lat_max - lat_min) / 30
     lng_window = (lng_max - lng_min) / 30
-    
+    return lat_min, lng_min, lat_window, lng_window
+
+def get_coord_mask(df, lat_min, lng_min, lat_window, lng_window):
     df_np = df[['lat', 'lng']].to_numpy()
     uniq_coords = np.unique(df_np, axis=0)
     # copy np array
@@ -32,7 +34,7 @@ def get_coord_mask(df):
     return coord_map
 
 import math
-def find_closest(coords):
+def get_closest_station(coords):
 
   closest = float('inf')
   closestLoc = ""
